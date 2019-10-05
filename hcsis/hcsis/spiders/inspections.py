e80b8f15ed9f42da2e3b3e227c9e99c13b8202e4
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-10-05 13:20:08 -0400

    add inspection report scraper func - as yet untested

--- hcsis/hcsis/spiders/inspections.py
+++ hcsis/hcsis/spiders/inspections.py
@@ -16,6 +16,9 @@ class InspectionsSpider(scrapy.Spider):
         provider_rows = response.css('blockquote td.BodyText')
 
         for count, row in enumerate(provider_rows):
+            # For testing purposes - delete this later
+            if count > 8:
+                break
             item = HcsisItem()
             provider_name = row.css('td a::text').extract_first()
             provider_id = row.css('td a::attr(href)').re_first('\d+$')
@@ -32,8 +35,8 @@ class InspectionsSpider(scrapy.Spider):
         next_page = f'https://www.hcsis.state.pa.us/hcsis-ssd/ServicesSupportDirectory/Providers/GetProviders' \
             f'?alphabet={InspectionsSpider.ALPHABET[InspectionsSpider.page_count]}'
 
-        # if InspectionsSpider.page_count > 0:
-        if InspectionsSpider.page_count < (len(InspectionsSpider.ALPHABET) - 1):
+        if InspectionsSpider.page_count > 0:
+        # if InspectionsSpider.page_count < (len(InspectionsSpider.ALPHABET) - 1):
             InspectionsSpider.page_count += 1
             yield response.follow(next_page, callback=self.parse)
 
@@ -51,10 +54,17 @@ class InspectionsSpider(scrapy.Spider):
         if location_rows:
             for location in location_rows:
                 service_location = location.css('::text').extract_first()
+                service_location_id = location.css('::attr(href)').re_first('\d+$')
                 item['service_location'] = service_location
-                yield item
+                item['service_location_id'] = service_location_id
+
+                location_inspection_page = f"https://www.hcsis.state.pa.us/hcsis-ssd/ssd/odp/pages/Inspections.aspx?p_varProvrId=1471&ServiceLocationID={service_location_id}"
+                if service_location_id:
+                    yield response.follow(location_inspection_page, callback=self.parse_inspection_page, meta={'item':
+                                                                                                                item})
         else:
             item['service_location'] = "No certified locations"
+            item['service_location_id'] = None
             yield item
 
         if pagination:
@@ -65,4 +75,62 @@ class InspectionsSpider(scrapy.Spider):
                     '__EVENTTARGET': 'ctl00$SSDPageContent$grdCertifiedServiceLocations',
                     '__EVENTARGUMENT': f'Page${page}',
                 }, callback=self.parse_cert_page, meta={'item': item, 'cert_page_count': page})
+
+
+    def parse_inspection_page(self, response):
+        item = response.meta.get('item')
+
+        rows = response.css('form div div table#grdInspections > tr')
+        # print(response.css('html').extract())
+        if rows:
+            rows = rows[1:] # remove col headers
+            print(f"LENGTH OF ROWS: {len(rows)}")
+            for count, row in enumerate(rows):
+                print(f"ROW {count}")
+                print(f"Column count: {len(row.css('tr > td').extract())}")
+                if row.css('div table'):
+                    # if the row is an inspection grid then do nothing because it will have already been saved in
+                    # prior loop iteration
+                    print("Contains inspection grid, moving on...")
+                    continue
+                else:
+                    # Save meta inspection info
+                    print("Saving inspection meta data...")
+                    inspection_id = row.css('td:nth-child(1) span::text').extract_first()
+                    inspection_reason = row.css('td:nth-child(2) span::text').extract_first()
+                    inspection_date = row.css('td:nth-child(3) span::text').extract_first()
+                    inspection_status = row.css('td:nth-child(4) span::text').extract_first()
+                    item['inspection_id'] = inspection_id
+                    item['inspection_reason'] = inspection_reason
+                    item['inspection_date'] = inspection_date
+                    item['inspection_status'] = inspection_status
+                if count < (len(rows) - 1):
+                    if rows[count + 1].css('div table'):
+                        print("Next row has inspection grid, saving inspection data...")
+                        inspection_grid_rows = rows[count + 1].css('div table tr')
+                        # loop over all rows except col headers
+                        for i_row in inspection_grid_rows[1:]:
+                            # save inspection data
+                            item['regulation'] = i_row.css('td:nth-child(1)::text').extract_first()
+                            item['non_compliance_area'] = i_row.css('td:nth-child(2)::text').extract_first()
+                            item['correction_required'] = i_row.css('td:nth-child(3)::text').extract_first()
+                            item['plans_of_correction'] = i_row.css('td:nth-child(4)::text').extract_first()
+                            item['correction_date'] = i_row.css('td:nth-child(5) span::text').extract_first()
+                            item['poc_status'] = i_row.css('td:nth-child(6)::text').extract_first()
+                            yield item
+                else:
+                    print("Next row has no inspection grid...")
+
+                    item['regulation'] = None
+                    item['non_compliance_area'] = None
+                    item['correction_required'] = None
+                    item['plans_of_correction'] = None
+                    item['correction_date'] = None
+                    item['poc_status'] = None
+                    yield item
+        else:
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print("~~~~~~~~ NO ROWS FOUND! ~~~~~~")
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 
