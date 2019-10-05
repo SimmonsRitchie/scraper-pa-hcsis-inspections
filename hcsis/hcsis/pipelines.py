c267308d00f481d02e3179ccd553877e963b58a8
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-10-05 13:28:25 -0400

    add new items

--- hcsis/hcsis/pipelines.py
+++ hcsis/hcsis/pipelines.py
@@ -20,7 +20,18 @@ class HcsisPipeline(object):
         self.curr.execute("""create table inspections_tb(
             provider_name text,
             provider_id text,
-            service_location text
+            service_location text,
+            service_location_id text
+            inspection_id text,
+            inspection_reason text,
+            inspection_date text,
+            inspection_status text,
+            regulation text,
+            non_compliance_area text,
+            correction_required text,
+            plans_of_correction text,
+            correction_date text,
+            poc_status text
             )""")
 
     def process_item(self, item, spider):
@@ -28,10 +39,21 @@ class HcsisPipeline(object):
         return item
 
     def store_db(self, item):
-        self.curr.execute("""insert into inspections_tb values (?, ?, ?)""", (
+        self.curr.execute("""insert into inspections_tb values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", (
             item['provider_name'],
             item['provider_id'],
-            item['service_location']
+            item['service_location'],
+            item['service_location_id'],
+            item['inspection_id'],
+            item['inspection_reason'],
+            item['inspection_date'],
+            item['inspection_status'],
+            item['regulation'],
+            item['non_compliance_area'],
+            item['correction_required'],
+            item['plans_of_correction'],
+            item['correction_date'],
+            item['poc_status']
                 )
             )
         self.conn.commit()
