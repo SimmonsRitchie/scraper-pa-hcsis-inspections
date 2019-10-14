15c623468c6228e4fc77d607d0b88ae0b7abfa9f
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-10-14 15:51:30 -0400

    disable pipeline

--- hcsis/hcsis/settings.py
+++ hcsis/hcsis/settings.py
@@ -69,9 +69,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'hcsis.pipelines.HcsisPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'hcsis.pipelines.HcsisPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
