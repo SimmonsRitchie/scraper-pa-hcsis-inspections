df8ac72396c0f3b6baae839d42280d89bdefb6d7
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-10-14 14:58:08 -0400

    add 8 new fields to db: region, county, etc.

--- hcsis/hcsis/pipelines.py
+++ hcsis/hcsis/pipelines.py
@@ -23,13 +23,25 @@ class HcsisPipeline(object):
             certified_locations_url text,
             service_location text,
             service_location_id text,
+            
+            region text,
+            county text,
+            service_specialty text,
+            address text,
+            first_cert_start_date text,
+            
+            first_cert_end_date text,
+            last_cert_start_date text,
+            last_cert_end_date text,
             inspections_page_url text,
             inspections_found text,
+            
             inspection_id text,
             inspection_reason text,
             inspection_date text,
             inspection_status text,
             regulation text,
+            
             non_compliance_area text,
             correction_required text,
             plans_of_correction text,
@@ -42,19 +54,37 @@ class HcsisPipeline(object):
         return item
 
     def store_db(self, item):
-        self.curr.execute("""insert into inspections_tb values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", (
+        self.curr.execute("""insert into inspections_tb values (
+        ?,?,?,?,?,
+        ?,?,?,?,?,
+        ?,?,?,?,?,
+        ?,?,?,?,?,
+        ?,?,?,?,?
+        )""", (
             item['provider_name'],
             item['provider_id'],
             item['certified_locations_url'],
             item['service_location'],
             item['service_location_id'],
+
+            item['region'],
+            item['county'],
+            item['service_specialty'],
+            item['address'],
+            item['first_cert_start_date'],
+
+            item['first_cert_end_date'],
+            item['last_cert_start_date'],
+            item['last_cert_end_date'],
             item['inspections_page_url'],
             item['inspections_found'],
+
             item['inspection_id'],
             item['inspection_reason'],
             item['inspection_date'],
             item['inspection_status'],
             item['regulation'],
+
             item['non_compliance_area'],
             item['correction_required'],
             item['plans_of_correction'],
